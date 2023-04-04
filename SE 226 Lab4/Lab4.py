647ba4e68e2065ccac9023f9ebb7c92ad0f2f418
Hasan Efe Dinç <102179658+HasanEfeDinc@users.noreply.github.com>
2023-04-04 12:09:28 +0300

    Add files via upload

---
+++ SE 226 Lab4/Lab4.py
@@ -0,0 +1,216 @@
+import random
+from typing import List, Union, Any
+
+words = ["a", "ability", "able", "about", "above", "accept", "according", "account", "across", "act", "action",
+         "activity", "actually", "add", "address", "administration", "admit", "adult", "affect", "after", "again",
+         "against", "age", "agency", "agent", "ago", "agree", "agreement", "ahead", "air", "all", "allow", "almost",
+         "alone", "along", "already", "also", "although", "always", "American", "among", "amount", "analysis", "and",
+         "animal", "another", "answer", "any", "anyone", "anything", "appear", "apply", "approach", "area", "argue",
+         "arm", "around", "arrive", "art", "article", "artist", "as", "ask", "assume", "at", "attack", "attention",
+         "attorney", "audience", "author", "authority", "available", "avoid", "away", "baby", "back", "bad", "bag",
+         "ball", "bank", "bar", "base", "be", "beat", "beautiful", "because", "become", "bed", "before", "begin",
+         "behavior", "behind", "believe", "benefit", "best", "better", "between", "beyond", "big", "bill", "billion",
+         "bit", "black", "blood", "blue", "board", "body", "book", "born", "both", "box", "boy", "break", "bring",
+         "brother", "budget", "build", "building", "business", "but", "buy", "by", "call", "camera", "campaign", "can",
+         "cancer", "candidate", "capital", "car", "card", "care", "career", "carry", "case", "catch", "cause", "cell",
+         "center", "central", "century", "certain", "certainly", "chair", "challenge", "chance", "change", "character",
+         "charge", "check", "child", "choice", "choose", "church", "citizen", "city", "civil", "claim", "class",
+         "clear", "clearly", "close", "coach", "cold", "collection", "college", "color", "come", "commercial", "common",
+         "community", "company", "compare", "computer", "concern", "condition", "conference", "Congress", "consider",
+         "consumer", "contain", "continue", "control", "cost", "could", "country", "couple", "course", "court", "cover",
+         "create", "crime", "cultural", "culture", "cup", "current", "customer", "cut", "dark", "data", "daughter",
+         "day", "dead", "deal", "death", "debate", "decade", "decide", "decision", "deep", "defense", "degree",
+         "Democrat", "democratic", "describe", "design", "despite", "detail", "determine", "develop", "development",
+         "die", "difference", "different", "difficult", "dinner", "direction", "director", "discover", "discuss",
+         "discussion", "disease", "do", "doctor", "dog", "door", "down", "draw", "dream", "drive", "drop", "drug",
+         "during", "each", "early", "east", "easy", "eat", "economic", "economy", "edge", "education", "effect",
+         "effort", "eight", "either", "election", "else", "employee", "end", "energy", "enjoy", "enough", "enter",
+         "entire", "environment", "environmental", "especially", "establish", "even", "evening", "event", "ever",
+         "every", "everybody", "everyone", "everything", "evidence", "exactly", "example", "executive", "exist",
+         "expect", "experience", "expert", "explain", "eye", "face", "fact", "factor", "fail", "fall", "family", "far",
+         "fast", "father", "fear", "federal", "feel", "feeling", "few", "field", "fight", "figure", "fill", "film",
+         "final", "finally", "financial", "find", "fine", "finger", "finish", "fire", "firm", "first", "fish", "five",
+         "floor", "fly", "focus", "follow", "food", "foot", "for", "force", "foreign", "forget", "form", "former",
+         "forward", "four", "free", "friend", "from", "front", "full", "fund", "future", "game", "garden", "gas",
+         "general", "generation", "get", "girl", "give", "glass", "go", "goal", "good", "government", "great", "green",
+         "ground", "group", "grow", "growth", "guess", "gun", "guy", "hair", "half", "hand", "hang", "happen", "happy",
+         "hard", "have", "he", "head", "health", "hear", "heart", "heat", "heavy", "help", "her", "here", "herself",
+         "high", "him", "himself", "his", "history", "hit", "hold", "home", "hope", "hospital", "hot", "hotel", "hour",
+         "house", "how", "however", "huge", "human", "hundred", "husband", "I", "idea", "identify", "if", "image",
+         "imagine", "impact", "important", "improve", "in", "include", "including", "increase", "indeed", "indicate",
+         "individual", "industry", "information", "inside", "instead", "institution", "interest", "interesting",
+         "international", "interview", "into", "investment", "involve", "issue", "it", "item", "its", "itself", "job",
+         "join", "just", "keep", "key", "kid", "kill", "kind", "kitchen", "know", "knowledge", "land", "language",
+         "large", "last", "late", "later", "laugh", "law", "lawyer", "lay", "lead", "leader", "learn", "least", "leave",
+         "left", "leg", "legal", "less", "let", "letter", "level", "lie", "life", "light", "like", "likely", "line",
+         "list", "listen", "little", "live", "local", "long", "look", "lose", "loss", "lot", "love", "low", "machine",
+         "magazine", "main", "maintain", "major", "majority", "make", "man", "manage", "management", "manager", "many",
+         "market", "marriage", "material", "matter", "may", "maybe", "me", "mean", "measure", "media", "medical",
+         "meet", "meeting", "member", "memory", "mention", "message", "method", "middle", "might", "military",
+         "million", "mind", "minute", "miss", "mission", "model", "modern", "moment", "money", "month", "more",
+         "morning", "most", "mother", "mouth", "move", "movement", "movie", "Mr", "Mrs", "much", "music", "must", "my",
+         "myself", "name", "nation", "national", "natural", "nature", "near", "nearly", "necessary", "need", "network",
+         "never", "new", "news", "newspaper", "next", "nice", "night", "no", "none", "nor", "north", "not", "note",
+         "nothing", "notice", "now", "n't", "number", "occur", "of", "off", "offer", "office", "officer", "official",
+         "often", "oh", "oil", "ok", "old", "on", "once", "one", "only", "onto", "open", "operation", "opportunity",
+         "option", "or", "order", "organization", "other", "others", "our", "out", "outside", "over", "own", "owner",
+         "page", "pain", "painting", "paper", "parent", "part", "participant", "particular", "particularly", "partner",
+         "party", "pass", "past", "patient", "pattern", "pay", "peace", "people", "per", "perform", "performance",
+         "perhaps", "period", "person", "personal", "phone", "physical", "pick", "picture", "piece", "place", "plan",
+         "plant", "play", "player", "PM", "point", "police", "policy", "political", "politics", "poor", "popular",
+         "population", "position", "positive", "possible", "power", "practice", "prepare", "present", "president",
+         "pressure", "pretty", "prevent", "price", "private", "probably", "problem", "process", "produce", "product",
+         "production", "professional", "professor", "program", "project", "property", "protect", "prove", "provide",
+         "public", "pull", "purpose", "push", "put", "quality", "question", "quickly", "quite", "race", "radio",
+         "raise", "range", "rate", "rather", "reach", "read", "ready", "real", "reality", "realize", "really", "reason",
+         "receive", "recent", "recently", "recognize", "record", "red", "reduce", "reflect", "region", "relate",
+         "relationship", "religious", "remain", "remember", "remove", "report", "represent", "Republican", "require",
+         "research", "resource", "respond", "response", "responsibility", "rest", "result", "return", "reveal", "rich",
+         "right", "rise", "risk", "road", "rock", "role", "room", "rule", "run", "safe", "same", "save", "say", "scene",
+         "school", "science", "scientist", "score", "sea", "season", "seat", "second", "section", "security", "see",
+         "seek", "seem", "sell", "send", "senior", "sense", "series", "serious", "serve", "service", "set", "seven",
+         "several", "sex", "sexual", "shake", "share", "she", "shoot", "short", "shot", "should", "shoulder", "show",
+         "side", "sign", "significant", "similar", "simple", "simply", "since", "sing", "single", "sister", "sit",
+         "site", "situation", "six", "size", "skill", "skin", "small", "smile", "so", "social", "society", "soldier",
+         "some", "somebody", "someone", "something", "sometimes", "son", "song", "soon", "sort", "sound", "source",
+         "south", "southern", "space", "speak", "special", "specific", "speech", "spend", "sport", "spring", "staff",
+         "stage", "stand", "standard", "star", "start", "state", "statement", "station", "stay", "step", "still",
+         "stock", "stop", "store", "story", "strategy", "street", "strong", "structure", "student", "study", "stuff",
+         "style", "subject", "success", "successful", "such", "suddenly", "suffer", "suggest", "summer", "support",
+         "sure", "surface", "system", "table", "take", "talk", "task", "tax", "teach", "teacher", "team", "technology",
+         "television", "tell", "ten", "tend", "term", "test", "than", "thank", "that", "the", "their", "them",
+         "themselves", "then", "theory", "there", "these", "they", "thing", "think", "third", "this", "those", "though",
+         "thought", "thousand", "threat", "three", "through", "throughout", "throw", "thus", "time", "to", "today",
+         "together", "tonight", "too", "top", "total", "tough", "toward", "town", "trade", "traditional", "training",
+         "travel", "treat", "treatment", "tree", "trial", "trip", "trouble", "true", "truth", "try", "turn", "TV",
+         "two", "type", "under", "understand", "unit", "until", "up", "upon", "us", "use", "usually", "value",
+         "various", "very", "victim", "view", "violence", "visit", "voice", "vote", "wait", "walk", "wall", "want",
+         "war", "watch", "water", "way", "we", "weapon", "wear", "week", "weight", "well", "west", "western", "what",
+         "whatever", "when", "where", "whether", "which", "while", "white", "who", "whole", "whom", "whose", "why",
+         "wide", "wife", "will", "win", "wind", "window", "wish", "with", "within", "without", "woman", "wonder",
+         "word", "work", "worker", "world", "worry", "would", "write", "writer", "wrong", "yard", "yeah", "year", "yes",
+         "yet", "you", "young", "your", "yourself"]
+#Q1
+cond = True
+cond2 = True
+while True:
+    while cond2:
+        first= int(input("Enter First Number with 1-999: "))
+        if first <= 999 and first >=1:
+            cond2 =False
+    if first <=999 and first >=1:
+        while cond:
+            second = int(input("Enter Second Number with 1-999 and greater than first: "))
+            if second > first and second <= 999 and second>=1:
+                cond = False
+
+    if first and second <=999 and first and second >=1:
+        break
+
+print("First number is " + str(first))
+print("Second number is " + str(second))
+
+new_list = words[first::second]
+
+#Q2
+while True:
+    user_number = int(input("Enter number form 3-7: "))
+    if user_number <8 and user_number >2:
+        break
+              #??py write this for print error
+random_words: list[Union[str, Any]] = random.sample(words,user_number)
+#print(random_words)
+
+password = ''.join(random_words)
+
+password = password[::-1]
+print(password)
+
+#Q3
+shorter = set()
+longer = set()
+
+for i in range(8):
+    while True:
+        user_number1 = int(input("Enter number from 3-7: "))
+        if user_number1 < 3 or user_number1 > 7:
+            print("Invalid input. Please enter a number from 3-7.")
+        else:
+            break
+    if user_number1 <5:
+        random_words1: list[Union[str, Any]] = random.sample(words, user_number1)
+        password1 = ''.join(random_words1)
+        password1 = password1[::-1]
+        shorter.add(password1)
+    else:
+        random_words1: list[Union[str, Any]] = random.sample(words, user_number1)
+        password1 = ''.join(random_words1)
+        password1 = password1[::-1]
+        longer.add(password1)
+
+print(shorter.union(longer))
+
+#Q4
+
+#4a
+dict = {}
+for i in range(1, 31):
+    if i == 1:
+        dict[i] = 0
+    else:
+        dict[i] = (i-1) * i
+print(dict)
+
+#4b
+dict1 = {}
+
+for i in range(1, 31):
+    if i == 1:
+        dict1[i] = 0
+    else:
+        dict1[i] = (i-1) * i
+
+for a,b in dict1.items():
+    print(f"{a}: {b}")
+
+#4c
+sumval = 0
+for values in dict.values():
+    sumval += values
+print(sumval)
+
+#4d
+
+while True:
+    delnumber = int(input("Please enter value in 1-31 "))
+    if delnumber <=31 and delnumber >=1:
+        break
+
+    dict.pop(delnumber)
+#Q5
+divided = {'Tony': 41, 'Rhodey': 43, 'Nat': 35}
+we_fall = {'Steve': 39, 'Clint': 35, 'Wanda': 28}
+#5a
+united_we_stand = {**divided,**we_fall}
+for element,value in united_we_stand.items():
+    print(f"{element}:{value}")
+
+#5b
+united_we_stand.pop("Nat")
+print(united_we_stand)
+
+#5c
+for key in sorted(united_we_stand.keys()):
+    print(key, united_we_stand[key])
+
+#5d
+maxval = max(united_we_stand.values())
+minval = min(united_we_stand.values())
+
+print("maximum value: " + str(maxval))
+print("minimum value: " + str(minval))
+
+
+
+
+
