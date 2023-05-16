15d23d23932ae35ab48fda15ff8251f20d65af24
Hasan Efe Dinç <102179658+HasanEfeDinc@users.noreply.github.com>
2023-05-16 11:24:30 +0300

    Add files via upload

---
+++ SE 226 Lab-8/Lab8.py
@@ -0,0 +1,49 @@
+#Q1
+from abc import ABC, abstractmethod
+from collections import Counter
+
+
+
+class abstract_class(ABC):
+    adress = "C:\\Users\\ekmek\\OneDrive\\Masaüstü\\Serbest Kod\\SE-226\\Lab8\\LAB8\\weirdWords.txt"
+    def __init__(self, adress):
+        self.adress = adress
+
+    @abstractmethod
+    def calculateFreqs(self):
+        pass
+
+#Q2-#Q3
+class ListCount(abstract_class):
+    def calculateFreqs(self):
+
+        with open(self.adress, 'r') as file:
+            characters = list(file.read())
+        frequencies = Counter(characters)
+        result = []
+        for char, freq in frequencies.items():
+            result.append(char)
+            result.append(freq)
+
+        print(result)
+
+
+class DictCount(abstract_class):
+    def calculateFreqs(self):
+        freqs = {}
+        with open(self.adress, 'r') as file:
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        char = char.lower()
+                        if char in freqs:
+                            freqs[char] += 1
+                        else:
+                            freqs[char] = 1
+        print(freqs)
+#Q4
+list_counter = ListCount("C:\\Users\\ekmek\\OneDrive\\Masaüstü\\Serbest Kod\\SE-226\\Lab8\\LAB8\\weirdWords.txt")
+list_counter.calculateFreqs()
+
+dict_counter = DictCount("C:\\Users\\ekmek\\OneDrive\\Masaüstü\\Serbest Kod\\SE-226\\Lab8\\LAB8\\weirdWords.txt")
+dict_counter.calculateFreqs()
